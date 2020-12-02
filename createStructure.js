3bf12edd45757db3f3a55be4456ed5c39df90f10
danillenname <55623401+danillenname@users.noreply.github.com>
2020-12-02 19:47:09 +0300

    Обработчик ошибок добавлен

--- createStructure.js
+++ createStructure.js
@@ -1,6 +1,19 @@
 let fs = require('fs');
-let structure = JSON.parse(fs.readFileSync('TestcaseStructure.json').toString());
-let values = JSON.parse(fs.readFileSync('Values.json').toString());
+try {
+	let structure = JSON.parse(fs.readFileSync('TestcaseStructure.json').toString());
+	let values = JSON.parse(fs.readFileSync('Values.json').toString());
+	structure.params.forEach(el => fillParams(el, values))
+	fs.writeFile("result.json", JSON.stringify(structure), (err) => {
+		if (err) throw err;
+	})
+}
+catch (err) {
+	let errorMessage = {"error": {"message": "Входные файлы некорректны"}}
+	fs.writeFile("error.json", JSON.stringify(errorMessage), (err) => {
+		if (err) throw err;
+	})
+	console.log(err.name)
+}
 
 function valueIdGiver(id, values) {
 	for (let i = 0; i < values.length; i++) {
@@ -16,21 +29,13 @@ function valueGiverFromValues(id, values) {
 	return '';
 }
 
-function fillParams(element) {
+function fillParams(element, values) {
+	console.log(values)
 	if (!element.values) element.value = valueIdGiver(element.id, values.values)
 	else {
 		element.values.forEach(el => {
-			if (el.params) el.params.forEach(el => fillParams(el))
+			if (el.params) el.params.forEach(el => fillParams(el, values))
 		});
 		element.value = valueGiverFromValues(valueIdGiver(element.id, values.values), element.values)
 	}
 }
-
-function createStructure() {
-	structure.params.forEach(el => fillParams(el))
-	fs.writeFile("result.json", JSON.stringify(structure), (err) => {
-		if (err) throw err;
-	})
-}
-
-createStructure()
